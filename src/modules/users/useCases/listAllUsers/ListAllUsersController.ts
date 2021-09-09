a105ec7f4d8ab58f2b830198e41536104240b211
Vinicius de Alcantra Barros <viniciusalcantarabarros@gmail.com>
2021-09-09 12:34:51 -0300

    Passando por testes

--- src/modules/users/useCases/listAllUsers/ListAllUsersController.ts
+++ src/modules/users/useCases/listAllUsers/ListAllUsersController.ts
@@ -6,7 +6,16 @@ class ListAllUsersController {
   constructor(private listAllUsersUseCase: ListAllUsersUseCase) {}
 
   handle(request: Request, response: Response): Response {
-    // Complete aqui
+    try {
+      const id = request.headers.user_id;
+
+      const user_id: string = id.toString();
+      const listUsers = this.listAllUsersUseCase.execute({ user_id });
+
+      return response.status(201).json(listUsers);
+    } catch (error) {
+      return response.status(400).json({ error: error.message });
+    }
   }
 }
 
